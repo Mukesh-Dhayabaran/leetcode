5ba84a0535a79090db2e31d1a5a247b26c4dcdd2
MUKESH D <mukeshdhayabaran@gmail.com>
2025-06-11 10:01:01 +0530

    Time: 2304 ms (5.05%), Space: 52.4 MB (70%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int[] indicate = new int[nums.length];
+        for(int i=0;i<nums.length-1;i++)
+        {
+            int freq = 1;
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    indicate[j] = 0;
+                    freq += 1;
+                }
+            }
+            indicate[i] = freq;
+        }
+
+        int max = 0,index=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(indicate[i] > max)
+            {
+                max = indicate[i];
+                index = i;
+            }
+        }
+
+        return nums[index];
+    }
+}
