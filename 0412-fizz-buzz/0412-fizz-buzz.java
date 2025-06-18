530f87ec2626f23b57ad58f6ac84a1a9a7ef8146
MUKESH D <mukeshdhayabaran@gmail.com>
2025-06-18 15:04:32 +0530

    Time: 1 ms (99.71%), Space: 45.2 MB (41.66%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,7 +3,7 @@ class Solution {
         List<String> nums = new ArrayList<>();
         for(int i=1;i<=n;i++)
         {
-            if(i%3==0 && i%5==0)nums.add("FizzBuzz");
+            if(i%15==0)nums.add("FizzBuzz");
             else if(i%3==0)nums.add("Fizz");
             else if(i%5==0)nums.add("Buzz");
             else nums.add(String.valueOf(i));
