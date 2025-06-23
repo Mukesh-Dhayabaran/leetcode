793aef56a4a4eaadaf344977871b339e354d0e13
MUKESH D <mukeshdhayabaran@gmail.com>
2025-06-23 14:26:21 +0530

    Time: 1 ms (74.49%), Space: 41.5 MB (26.97%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -2,7 +2,7 @@ class Solution {
     public String convertToBase7(int num) {
         int n=num;
         if(num==0) return "0";
-        StringBuilder b = new StringBuilder("");
+        StringBuilder b = new StringBuilder();
         num=Math.abs(num);
 		while(num!=0)
 		{
