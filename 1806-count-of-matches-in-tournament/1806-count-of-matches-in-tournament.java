4f8ecb40c6c6d1f3a7ea83004606a206bd86516c
MUKESH D <mukeshdhayabaran@gmail.com>
2025-07-15 12:15:21 +0530

    Time: 0 ms (100%), Space: 40.5 MB (52.47%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
@@ -1,5 +1,13 @@
 class Solution {
     public int numberOfMatches(int n) {
-        return n-1;
+        // return n-1;
+        int sum = 0;
+        while(n!=1)
+        {
+            sum += (n/2);
+            n -= (n/2);
+        }
+
+        return sum;
     }
 }
