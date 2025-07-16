e671e5f33ae42100b8f058d8b6bad7f415ce299c
MUKESH D <mukeshdhayabaran@gmail.com>
2025-07-16 13:13:09 +0530

    Time: 1 ms (69.28%), Space: 40.5 MB (68.93%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.java
+++ 2083-three-divisors/2083-three-divisors.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isThree(int n) {
-        int count = 1;
-        for(int i=1;i<=n/2;i++)
+        int count = 2;
+        for(int i=2;i<=n/2;i++)
         if(n%i==0)
         count++;
         return count==3;
