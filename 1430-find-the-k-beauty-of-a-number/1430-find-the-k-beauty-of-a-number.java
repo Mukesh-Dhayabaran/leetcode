36ab170c2b275e49bd01669c18004f12a51def05
MUKESH D <mukeshdhayabaran@gmail.com>
2025-07-17 09:22:57 +0530

    Time: 0 ms (100%), Space: 40.6 MB (59.79%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.java
@@ -3,8 +3,11 @@ class Solution {
         String s = String.valueOf(num);
         int count = 0;
         for(int i=0;i<s.length()-(k-1);i++)
-        if(Integer.parseInt(s.substring(i,i+k))!=0 && num % Integer.parseInt(s.substring(i,i+k)) == 0)
-        count++;
+        {
+            int n = Integer.parseInt(s.substring(i,i+k));
+            if(n != 0 && num % n == 0)
+            count++;
+        }
         return count;
     }
 }
