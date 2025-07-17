b977668664cb661838c7f74e83b86b2c9af93169
MUKESH D <mukeshdhayabaran@gmail.com>
2025-07-17 16:00:13 +0530

    Time: 1 ms (38.35%), Space: 44.2 MB (89.55%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -20,12 +20,9 @@ class Solution {
         int min = Integer.MAX_VALUE;
         for(int i=0;i<list.size()-1;i++)
         {
-            for(int j=i+1;j<list.size();j++)
-            {
-                int n = Math.abs(list.get(i)-list.get(j));
+                int n = Math.abs(list.get(i)-list.get(i+1));
                 if(n < min)
                 min = n;
-            }
         }
         return min;
     }
