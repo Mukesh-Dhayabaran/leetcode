682f6bd77039074f19082d56baad8b018f5d6d97
MUKESH D <mukeshdhayabaran@gmail.com>
2025-07-29 13:14:46 +0530

    Time: 4 ms (77.58%), Space: 44.7 MB (34.56%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return new String(ch1).equals(new String(ch2));
+    }
+}
