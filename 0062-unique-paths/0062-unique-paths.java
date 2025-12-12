598c93bdf3c0b19408b0a3debc6c692be1b41aff
MUKESH D <mukeshdhayabaran@gmail.com>
2025-12-12 11:03:11 +0530

    Time: 0 ms (100%), Space: 41.5 MB (68.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,7 +4,7 @@ class Solution {
     for(int i=0;i<m;i++) Arrays.fill(memo[i],-1);
     return paths_count(0,0,m,n,memo);
   }
-//   public void paths_count(int row,int col,int m,int n)
+  
   public int paths_count(int row,int col,int m,int n,int [][] memo)
   {
     if(row == m || col == n) return 0;
